331c2142756c278cfd68ad90c6a3f0f868f5392f
allison_johnson <allison.cuttler@gmail.com>
2019-12-16 12:21:34 -0800

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
+    attr_accessor :name, :age
   
-end
+    @@all = []
+  
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def new_meal(waiter, total, tip = 0)
+      Meal.new(waiter, self, total, tip)
+    end #new_meal
+
+    def meals   
+      Meal.all.select {|meal| meal.customer == self}
+    end #meals
+
+    def waiters
+      meals.collect {|meal| meal.waiter}
+    end #waiters
+  
+  end
