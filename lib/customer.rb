e68477177b48c59e3375419ea508f2a97c7209ba
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-01-10 11:26:50 -0800

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,52 @@
 class Customer
+    attr_accessor :name, :age
+
+    @all = [] #the customer class should know of every customer instance. 
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end #rachel = Customer.new("Rachel", 27)
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip) #don't need to take a customer in as an argument b/c we are passing ourself
+    end #rachel.new_meal(dan, 50, 10)
+
+    def meals #customer will look at all of the meals & select the ones that belong to them
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end #call like -> rachel.meals #rachel.meals.length
+
+    def waiters
+        meals.map do |meal|
+            meal.waiter
+        end
+    end #rachel.waiters  # rachel.waiters.length #rachel.waiters.last.name
   
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def new_meal_20_percent(waiter, total)
+        tip = total * 0.2
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def self.oldest_customer
+        oldest_age = 0
+        oldest_customer = nil
+        self.all.each do |customer|
+            if customer.age > oldest_age
+                oldest_age = customer.age
+                oldest_customer = customer
+            end
+        end
+        oldest_customer
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,48 @@
 class Waiter
 
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip) #don't need to take a waiter in as an argument b/c we are passing ourself
+    end #dan.new_meal(rachel, 50, 10)
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+            meal_a.tip <=> meal_b.tip
+        end
+        best_tipped_meal.customer
+    end # waiter.best_tipper OR waiter.best_tipper.name
+
+    def most_frequent_customer #waiter's most frequent customer
+    end
+
+    def meal_of_worst_tipped_customer #meal of worst tipping customer
+        worst_tipped_meal = meals.max do |meal_a, meal_b|
+            meal_b.tip <=> meal_a.tip
+        end
+        worst_tipped_meal.customer.meal
+    end
+
+    def average_tips # average tips of most and least experienced customer
+    end
+
+
 end
